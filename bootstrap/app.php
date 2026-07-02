a5722356db58dba5c268e0ca24bbcaa2d190b8b0
RenzArcilla <renzarcilla09@gmail.com>
2026-07-02 17:56:21 +0800

    fix: trust reverse proxy headers for correct HTTPS asset URLs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                    Request::HEADER_X_FORWARDED_HOST |
+                    Request::HEADER_X_FORWARDED_PORT |
+                    Request::HEADER_X_FORWARDED_PROTO
+        );
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
